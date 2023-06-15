e90ab4e0a65db1a10bb4cf1511661c7de7e26b19
jdaeL <joedaniel0804@gmail.com>
2023-06-14 22:54:43 -0500

    agregando paths a urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('', views.listar, name='listar'),
+    path('crear/', views.crear, name='crear'),
+    path('editar/<int:pk>/', views.editar, name='editar'),
+    path('eliminar/<int:pk>/', views.eliminar, name='eliminar'),
     path('style.css', views.style)
+
 
 ]
