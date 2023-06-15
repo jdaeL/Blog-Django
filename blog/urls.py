6197e20695615e77a4aea90f9df10c0d637c5d2c
jdaeL <joedaniel0804@gmail.com>
2023-06-14 22:57:07 -0500

    paths a urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -3,11 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('', views.listar, name='listar'),
-    path('crear/', views.crear, name='crear'),
-    path('editar/<int:pk>/', views.editar, name='editar'),
-    path('eliminar/<int:pk>/', views.eliminar, name='eliminar'),
+
+    path('', views.listar_articulos, name='listar'),
+    path('crear/', views.crear_articulo, name='crear'),
+    path('editar/<int:pk>/', views.editar_articulo, name='editar'),
+    path('eliminar/<int:pk>/', views.eliminar_articulo, name='eliminar'),
     path('style.css', views.style)
-
-
-]
+]   
