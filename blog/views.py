b784cc0aa7846e26970087419eba8ace2d8c1b6f
jdaeL <joedaniel0804@gmail.com>
2023-06-14 22:47:55 -0500

    funciones en views.py

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from django.http import HttpResponse
@@ -15,3 +15,39 @@ def style(request):
     response['Content-Disposition'] = 'attachment; filename="style.css"'
     response.write(render(request, 'blog/style.css', {'posts': posts}).content)
     return response
+
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Post
+
+def listar_articulos(request):
+    articulos = Post.objects.all()
+    return render(request, 'blog/listar_articulos.html', {'articulos': articulos})
+
+def crear_articulo(request):
+    if request.method == 'POST':
+        titulo = request.POST['titulo']
+        texto = request.POST['texto']
+        autor = request.user  # Obtener el usuario actual (requiere autenticación)
+        articulo = Post(author=autor, title=titulo, text=texto, created_date=timezone.now())
+        articulo.save()
+        return redirect('listar_articulos')
+    return render(request, 'blog/crear_articulo.html')
+
+def editar_articulo(request, pk):
+    articulo = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        titulo = request.POST['titulo']
+        texto = request.POST['texto']
+        articulo.title = titulo
+        articulo.text = texto
+        articulo.save()
+        return redirect('listar_articulos')
+    return render(request, 'blog/editar_articulo.html', {'articulo': articulo})
+
+def eliminar_articulo(request, pk):
+    articulo = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        articulo.delete()
+        return redirect('listar_articulos')
+    return render(request, 'blog/eliminar_articulo.html', {'articulo': articulo})
