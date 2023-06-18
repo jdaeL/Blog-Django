c5adee581717e4e4b2dded4da249ad61acae7a50
jdaeL <joedaniel0804@gmail.com>
2023-06-17 22:42:19 -0500

    Agregando rutas nuevas

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
 
-    path('', views.listar_articulos, name='listar'),
-    path('crear/', views.crear_articulo, name='crear'),
-    path('editar/<int:pk>/', views.editar_articulo, name='editar'),
-    path('eliminar/<int:pk>/', views.eliminar_articulo, name='eliminar'),
+    path('post/<int:pk>/', views.listar_articulos, name='listar_articulos'),
+    path('post/new', views.crear_articulo, name='crear_articulo'),
+    path('post/<int:pk>/edit/', views.editar_articulo, name='editar_articulo'),
+    path('post/<int:pk>/delete/', views.eliminar_articulo, name='eliminar_articulo'),
+    path('new/', views.crear_articulo, name='crear_articulo'),
+
+
     path('style.css', views.style)
 ]   
